8d7aadf55d8dbc7ac085c26b4bbadf8bdc72cd53
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2020-01-10 10:33:12 +0100

    added command line options for resuming from an existing model

--- run_training.py
+++ run_training.py
@@ -33,7 +33,7 @@ _valid_configs = [
 
 #----------------------------------------------------------------------------
 
-def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, mirror_augment, metrics):
+def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, mirror_augment, metrics, resume_pkl, resume_kimg):
     train     = EasyDict(run_func_name='training.training_loop.training_loop') # Options for training loop.
     G         = EasyDict(func_name='training.networks_stylegan2.G_main')       # Options for generator network.
     D         = EasyDict(func_name='training.networks_stylegan2.D_stylegan2')  # Options for discriminator network.
@@ -50,6 +50,11 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.total_kimg = total_kimg
     train.mirror_augment = mirror_augment
     train.image_snapshot_ticks = train.network_snapshot_ticks = 10
+
+    train.resume_pkl = resume_pkl #'/home/beniz/apps/stylegan2/results/00002-stylegan2-a_256x256-4gpu-config-f/network-snapshot-005725.pkl'
+    train.resume_kimg = resume_kimg #5814.3
+    train.resume_time = 0.0
+    
     sched.G_lrate_base = sched.D_lrate_base = 0.002
     sched.minibatch_size_base = 32
     sched.minibatch_gpu_base = 4
@@ -168,6 +173,8 @@ def main():
     parser.add_argument('--gamma', help='R1 regularization weight (default is config dependent)', default=None, type=float)
     parser.add_argument('--mirror-augment', help='Mirror augment (default: %(default)s)', default=False, metavar='BOOL', type=_str_to_bool)
     parser.add_argument('--metrics', help='Comma-separated list of metrics or "none" (default: %(default)s)', default='fid50k', type=_parse_comma_sep)
+    parser.add_argument('--resume-pkl', help='Path to model pkl to resume from', default=None)
+    parser.add_argument('--resume-kimg', help='Number of images / steps to resume from', default=0.0, type=float)
 
     args = parser.parse_args()
 
